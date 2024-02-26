8f716a529e3d5f4a0250328548ac940557b58b13
J4g3rWulf <J4g3rWulf@users.noreply.github.com>
2024-02-26 15:25:00 -0300

    altered patrimony.xml and patrimony.py

--- registers/models/patrimony.py
+++ registers/models/patrimony.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields, _
+from odoo import models, fields
 
 class Patrimony(models.Model):
     _name = "patrimony"
@@ -6,24 +6,25 @@ class Patrimony(models.Model):
 
     name = fields.Char(string='Nome', required=True)
 
+
     description = fields.Char(string='Descrição', required=False)
 
-    classification = fields.Selection( [('vehicles', 'Veículos'), 
-                                        ('heavies', 'Equipamento Pesado'), 
-                                        ('others', 'Outros')], 
-                                        string = 'Classificação')
-    
+    classification = fields.Selection([('vehicles', 'Veículos'),
+                                        ('heavies', 'Equipamento Pesado'),
+                                        ('others', 'Outros')
+                                        ], string = 'Classificação')
+
     vehicle_plate = fields.Char(string='Placa do Veículo', required=False)
 
-    renavan = fields.Integer(string='Renavan')
+    renavan = fields.Char(string='Renavan', required=False)
 
     heavy_type = fields.Selection([('escavadeira', 'Escavadeira'),
                                    ('retro_escavadeira', 'Retro Escavadeira'),
                                    ('pa_mecanica', 'Pá Mecânica'),
                                    ('triturador_galhos', 'Triturador de Galhos'),
                                    ('s90', 'S90'),
-                                   ])
-    
+                                    ], string='pesado_type', required=False)
+
     heavy_number = fields.Selection([('1', 'Número 1'),
                                      ('2', 'Número 2'),
                                      ('3', 'Número 3'),
@@ -34,10 +35,19 @@ class Patrimony(models.Model):
                                      ('8', 'Número 8'),
                                      ('9', 'Número 9'),
                                      ('10', 'Número 10'),
-                                     ])
-    
-    
+                                    ], string="pesado_num")
 
+    def save(self):
 
+        vals = {
+            'name': self.name,
+            'description': self.description,
+            'classification': self.classification,
+            'vehicle_plate': self.vehicle_plate,
+            'renavan': self.renavan,
+            'heavy_type': self.heavy_type,
+            'heavy_number': self.heavy_number
+        }
 
-
+        self.env['patrimony'].write(vals)
+        return {'type': 'ir.actions.act_window_close'}
