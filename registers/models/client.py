c8f8b9bb8787a18b77583898914c3b782fb907d7
J4g3rWulf <J4g3rWulf@users.noreply.github.com>
2024-03-06 15:31:45 -0300

    add client.py custom saving function

--- registers/models/client.py
+++ registers/models/client.py
@@ -9,3 +9,13 @@ class Client(models.Model):
     name = fields.Char(string='Nome', required=True)
     age = fields.Integer(string='Idade', required=True)
     obs = fields.Text(string='Obs', required=False)
+
+    def create_client(self):
+        """This is the custom function for saving an 'client' object"""
+        vals = {
+            'name': self.name,
+            'age': self.age,
+            'email': self.obs,
+        }
+
+        self.env['client'].write(vals)
