d0438d14bcf7c228d6d6349dd8dc52086f41e1ef
J4g3rWulf <J4g3rWulf@users.noreply.github.com>
2024-07-09 17:49:13 -0300

    bills: status_change updated

--- registers/models/bill.py
+++ registers/models/bill.py
@@ -8,8 +8,6 @@ import pytz
 class Bill(models.Model):
     """Fields and functions for the bill object"""
 
-
-
     def _generate_bill_id(self):
         """Function to generate an ID for the bill object"""
         return uuid4().node
@@ -19,7 +17,6 @@ class Bill(models.Model):
         user_tz = pytz.timezone(self.env.context.get('tz') or self.env.user.tz)
         date_today = pytz.utc.localize(datetime.now()).astimezone(user_tz)
         return date_today.date()
-
 
     _name = "bill"
     _description = "Reistro de Contas."
@@ -32,6 +29,7 @@ class Bill(models.Model):
     value = fields.Float(string='Valor', required=True)
     origin = fields.Selection([('is_cpf', 'Funcionário'), ('is_cnpj', 'Fornecedor')],
                                                     string='Fonte', required=False)
+    bill_status = fields.Char(string='Status da Conta', default='Provisória')
     name = fields.Char(string='Nome', required=False)
     cpf = fields.Char(string='CPF', required=False)
     cnpj = fields.Char(string='CNPJ', required=False)
@@ -39,9 +37,9 @@ class Bill(models.Model):
 
     def create_bill(self):
         """This is the custom function for saving a 'bill' object"""
-        if self.origin == 'is_cpf':
+        if self.origin == "is_cpf":
             self.cnpj = ''
-        elif self.origin == 'is_cnpj':
+        elif self.origin == "is_cnpj":
             self.cpf = ''
         else:
             self.cpf = ''
@@ -56,6 +54,7 @@ class Bill(models.Model):
             'description': self.description,
             'value': self.value,
             'origin': self.origin,
+            'bill_status': self.bill_status,
             'name': self.name,
             'cpf': self.cpf,
             'cnpj': self.cnpj,
@@ -74,6 +73,23 @@ class Bill(models.Model):
             },
         }
 
+    def update_bill_status(self):
+        if self.bill_status == 'Provisória':
+            self.bill_status = 'Autorizada'
+        elif self.bill_status == 'Autorizada':
+            self.bill_status = 'Paga'
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': _("Sucesso"),
+                'type': 'success',
+                'message': f'O status foi atualizado para {self.bill_status}!',
+                'sticky': False,
+            },
+        }
+
     @api.constrains('value')
     def _validate_value(self):
         """Checks if value field is a negative number or zero"""
@@ -85,7 +101,7 @@ class Bill(models.Model):
     def _validate_cpf(self):
         """Checks size of the CPF variable to limit different lengths"""
         for rec in self:
-            if rec.cpf:
+            if rec.cpf and self.origin == "is_cpf":
                 if len(rec.cpf) != 11:
                     raise ValidationError(_("O campo 'CPF' está com o tamanho incorreto. "
                                             "Precisa de 11 dígitos"))
@@ -97,7 +113,7 @@ class Bill(models.Model):
     def _validate_cnpj(self):
         """Checks size of the CPNJ variable to limit different lengths"""
         for rec in self:
-            if rec.cnpj:
+            if rec.cnpj and self.origin == "is_cnpj":
                 if len(rec.cnpj) != 14:
                     raise ValidationError(_("O campo 'CNPJ' está está com o tamanho incorreto. "
                                                 "Precisa de 8 dígitos"))
@@ -107,5 +123,6 @@ class Bill(models.Model):
 
     @api.constrains('bill_file')
     def _check_file(self):
-        if str(self.filename.split(".")[1]) != 'pdf' :
-                raise ValidationError("O sistema aceita apenas arquivos '.pdf'.")
+        if self.filename:
+            if str(self.filename.split(".")[1]) != 'pdf' :
+                    raise ValidationError("O sistema aceita apenas arquivos '.pdf'.")
