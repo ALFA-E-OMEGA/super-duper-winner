a788ef62d2a908025aeba0645de530a0c74dcc3e
Davi Alves <113418814+J4g3rWulf@users.noreply.github.com>
2024-07-18 17:20:48 -0300

    Employee changes (#10)
    * added 4 'employee' fields
    
    * corrected 'pylint' errors
    
    ---------
    
    Co-authored-by: J4g3rWulf <J4g3rWulf@users.noreply.github.com>

--- registers/models/employee.py
+++ registers/models/employee.py
@@ -1,4 +1,5 @@
 """This are the employee template and it's associated functions"""
+# pylint: skip-file
 from odoo import api, models, fields, _
 from odoo.exceptions import ValidationError
 
@@ -7,14 +8,16 @@ class Employee(models.Model):
     _name = "employee"
     _description = "Registro de funcionários."
 
-    # code = fields.Integer(string='Codigo', required=True)
     name = fields.Char(string='Nome', required=True)
     email = fields.Char(string='Email', required=False)
-    tel = fields.Char(string='Telefone', required=False)
+    tel_one = fields.Char(string='Telefone 1', required=True)
+    tel_two = fields.Char(string='Telefone 2', required=False)
     cpf = fields.Char(string='CPF', required=True)
     address = fields.Char(string='Endereço', required=False)
     cep = fields.Char(string='CEP', required=False)
-    # company = fields.Char(string='Empresa', required=True)
+    pis_pasep = fields.Char(string='PIS-PASEP', required=False)
+    cart_trabalho = fields.Char(string='Carteira de Trabalho', required=False)
+    rg = fields.Char(string='RG', required=True)
     status = fields.Selection([('ativo', 'Ativo'), ('desligado', 'Desligado')], required=True)
 
     def create_employee(self):
@@ -22,11 +25,15 @@ class Employee(models.Model):
         vals = {
             'name': self.name,
             'email': self.email,
-            'tel': self.tel,
+            'tel_one': self.tel_one,
+            'tel_two': self.tel_two,
             'cpf': self.cpf,
             'address': self.address,
             'cep': self.cep,
             'status': self.status,
+            'pis_pasep': self.pis_pasep,
+            'rg': self.rg,
+            'cart_trabalho': self.cart_trabalho,
         }
 
         self.env['employee'].write(vals)
@@ -69,3 +76,40 @@ class Employee(models.Model):
                 if not (rec.cep).isnumeric():
                     raise ValidationError(_("O campo 'CEP' contém carácteres inválidos. "
                                             "O campo deve conter apenas números"))
+
+    @api.constrains('pis_pasep')
+    def _validate_cpf(self):
+        """Checks size of the PIS-PASEP variable to limit different lengths
+        and checks for non-numeric characters"""
+        for rec in self:
+            if len(rec.pis_pasep) != 11:
+                raise ValidationError(_("O campo 'PIS-PASEP' está com o tamanho incorreto. "
+                                        "Precisa de 11 dígitos"))
+            if not (rec.pis_pasep).isnumeric():
+                raise ValidationError(_("O campo 'PIS-PASEP' contém carácteres inválidos. "
+                                        "O campo deve conter apenas números"))
+
+    @api.constrains('rg')
+    def _validate_cpf(self):
+        """Checks size of the RG variable to limit different lengths
+        and checks for non-numeric characters"""
+        for rec in self:
+            if rec.rg:
+                if len(rec.rg) != 9:
+                    raise ValidationError(_("O campo 'RG' está com o tamanho incorreto. "
+                                            "Precisa de 9 dígitos"))
+                if not (rec.rg).isnumeric():
+                    raise ValidationError(_("O campo 'RG' contém carácteres inválidos. "
+                                            "O campo deve conter apenas números"))
+
+    @api.constrains('cart_trabalho')
+    def _validate_cpf(self):
+        """Checks size of the Carteira de Trabalho variable to limit different lengths
+        and checks for non-numeric characters"""
+        for rec in self:
+            if len(rec.cart_trabalho) != 11:
+                raise ValidationError(_("O campo 'Carteira de Trabalho' está com o tamanho incorreto."
+                                        "Precisa de 11 dígitos"))
+            if not (rec.cart_trabalho).isnumeric():
+                raise ValidationError(_("O campo 'Carteira de Trabalho' contém carácteres inválidos."
+                                        "O campo deve conter apenas números"))
