7e5e314fc66b63c4f8295e076198d446fb687b6e
Davi Alves <J4g3rWulf@users.noreply.github.com>
2024-10-16 21:58:21 -0300

    re-added _sql_constraints, fixed visual bugs | cost_center and service

--- registers/models/employee.py
+++ registers/models/employee.py
@@ -142,3 +142,8 @@ class Employee(models.Model):
                 if not (rec.tel_two).isnumeric():
                     raise ValidationError(_("O campo 'Telefone 2' contém carácteres inválidos."
                                             "O campo deve conter apenas números"))
+
+    _sql_constraints = [
+        ('cpf_employee_unique', 'UNIQUE(cpf)', 'Já existe um \'Funcionário\' com esse CPF.')
+    ]
+    
