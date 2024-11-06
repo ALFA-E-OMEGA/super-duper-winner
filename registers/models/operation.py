f6d4b285a0632b2b162381cbb04bf8fe23ea63af
Davi Alves <J4g3rWulf@users.noreply.github.com>
2024-11-06 20:43:29 -0300

    changed 'operation' logic | one per day, can be reopened during the day

--- registers/models/operation.py
+++ registers/models/operation.py
@@ -8,41 +8,80 @@ class Operation(models.Model):
     """Fields and functions for the operation object"""
     _name = "operation"
     _description = "Registro de Caixa."
+    _rec_name = "operation_date"
 
     def _generate_register_date(self):
         """Function to generate current date based on user timezone"""
         user_tz = pytz.timezone(self.env.context.get('tz') or self.env.user.tz)
         date_today = pytz.utc.localize(datetime.now()).astimezone(user_tz)
         return date_today.date()
-
-    def _generate_id_operation(self):
-        current_date = self._generate_register_date()
-        
-        num_of_records = self.env['operation'].search_count([('operation_date', '=', current_date)])
-
-        if num_of_records == 0:
-            return 'Abertura'
-        elif num_of_records == 1:  
-            return   'Reabertura'
-        else:
-            return 'Operação Inválida'
         
     operation_date = fields.Date(string='Data de Registro', default=_generate_register_date)
-    id_operation = fields.Char(string='Operação', required=True, default=_generate_id_operation)
+    operation_status = fields.Selection([('1', 'Aberto'), ('0', 'Fechado')],
+                                       string="Status de Caixa", required=True,
+                                       default='1')
+    reopen_reason = fields.Text(string='Razão de Reabertura', required=False)
+    is_editable = fields.Boolean(string='Editável', required=True, compute='compute_is_editable',
+                                 default=True)
 
     def create_operation(self):
         """This is the custom function for saving an 'operation' object"""
         vals = {
             'operation_date': self.operation_date,
-            'id_operation': self.id_operation,
+            'operation_status': self.operation_status,
+            'reopen_reason': self.reopen_reason,
+            'is_editable': self.is_editable,
             }
 
         self.env['operation'].write(vals)
+    
+    def close_operation(self):
+        """This function closes the operation status and locks editing the file"""
+        self.operation_status = '0'
 
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': _("Sucesso"),
+                'type': 'success',
+                'message': _('Caixa fechado com sucesso!'),
+                'sticky': False,
+                'next': {
+                    'type': 'ir.actions.act_window_close',
+                }
+            },
+        }
+    
+    def reopen_operation(self):
+        """This function re-opens the operation and updates the 'Reason' field to true"""
+        self.operation_status = '1'
 
-    @api.constrains('id_operation')
-    def _validate_id_operation(self):
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': _("Sucesso"),
+                'type': 'success',
+                'message': _('Caixa reaberto com sucesso!'),
+                'sticky': False,
+                'next': {
+                    'type': 'ir.actions.act_window_close',
+                }
+            },
+        }
+    
+    def compute_is_editable(self):
+        """Function only allows altering the operation in it's current date"""
+        current_date = self._generate_register_date()
         for rec in self:
-            if rec.id_operation == 'Operação Inválida':
-                raise ValidationError(_("O sistema permite apenas 2 operações de caixa por dia."))
+            if rec.operation_date == current_date:
+                rec.is_editable = True
+            else:
+                rec.is_editable = False
+
+    _sql_constraints = [
+        ('operation_date_unique', 'UNIQUE(operation_date)',
+         'Já existe um \'Caixa\' nesta \'Data\'.')
+    ]
 
